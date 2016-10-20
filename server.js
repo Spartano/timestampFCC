99d98d603f80289ad0bd744a17362cab8a64ae11
Torac Victor <nasutz8@gmail.com>
2016-10-20 21:21:26 +0000

    Finished version

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 var express = require('express');
 var app = express();
+var moment = require("moment");
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var path = require('path');
+
+var port = process.env.PORT || 3500;
+
+
+app.get('/', function(req, res){
+    
+    var fileName = path.join(__dirname, 'index.html');
+    
+    res.sendFile(fileName, function(err){
+        if(err){
+            console.error(err)
+        }else{
+            console.log('Home page')
+        }
+    })
+    
+})
+
+app.get('/:unix', function (req, res) {
+    
+    var date = moment.utc( req.params.unix );
+   
+   if(date.isValid()) {
+        res.json( { "unix": date.valueOf(), "natural": moment(req.params.unix).format("MMMM DD, YYYY") } );
+   }else{
+       res.json({ 'unix': null, 'natural': null})
+   } 
+   
+   
+    
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+
+
+
+app.listen(port, function(){
+    
+    console.log('Listening to port ' + port)
+})
